38e4a4796a36bd2783e1d2af6f63add2dc8c26f5
Dan920-Dev <00138218@uca.edu.sv>
2022-05-08 22:03:04 -0600

    Update Readme and Add main 3, main 4

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ Repositorio de Ejercicios en C++ - Fundamentos
 
 
 ## Temas
-1. Entrada y Salida de datos
-2. Condicionales
-3. Estructuras repetitivas
-4. Arreglos o Vectores
-5. Matrices
+1. [Entrada y Salida de datos](/Scripts/main1.cpp) 
+2. [Variables](/Scripts/main2)
+3. [Operadores](/Scripts/main3.cpp)
+4. [Condicionales](/Scripts/main4.cpp)
+4. Estructuras repetitivas
+5. Arreglos o Vectores
+6. Matrices
 
 ## Ejercicios
 
---
+++ Scripts/main3.cpp
@@ -0,0 +1,38 @@
+// Operadores
+
+// En c++ podemos hacer uso de los siguientes operadores:
+
+// Operadores aritmeticos
+
+/*  
+
++  (Como signo o como Operacion Suma)
+-  (Como signo o como Operacion Resta)
+*  (Multiplicacion)
+/  (Division)
+%  (Resto)
+
+*/
+
+// Operadores relacionales
+
+/* 
+
+==  (Igual)
+!=  (Distinto)
+<  (Menor que)
+>  (Mayor que)
+<= (Menor o igual que)
+>= (Mayor o igual que)
+
+*/
+
+// Operadores Logicos
+
+/* 
+
+&& Y  (Retorna verdadero si Todo se cumple)
+! NOT (Retorna falso si el resultado es verdadero)
+or  O  (Retorna verdadero si alguna de todas se cumple)
+ 
+*/
