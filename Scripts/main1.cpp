f5d89aced8390041f6e73f52b89ae42d4d7b7436
Daniela897 <00136918@uca.edu.sv>
2022-05-08 21:19:28 -0600

    Add: main 1 - Entrada & Salida de datos

---
+++ Scripts/main1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Funcion principal del programa
+int main() {
+
+    // Declaracion de variables necesarias
+    int num;
+
+    // Uso de cout - Salida de datos
+    cout << "Hola, Ingrese un numero: " << endl; // endl hace un salto de linea
+
+    // Uso de cin - Entrada de datos
+    cin >> num; // Obtenemos el numero que el usuario ingrese
+
+    // Hacemos una salida de datos con cout y mostramos el numero
+    cout << "El numero ingresado es: " << num << endl; 
+    // Usamos el operador de suma para CONCATENAR
+
+    return 0;
+}
