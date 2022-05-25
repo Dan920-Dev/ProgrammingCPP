42ad016b44f2de0d7a8f1e5557342659a6f2a853
Dan920-Dev <00138218@uca.edu.sv>
2022-05-25 11:46:50 -0600

    Add main 17

--- Scripts/main17.cpp
+++ Scripts/main17.cpp
@@ -10,5 +10,43 @@ using namespace std;
 
 int main() {
 
+    cout << "*** Usando for ***" << endl;
+
+    int suma = 0, cont = 0, valor;
+
+    // Usando for
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "ingrese el valor Num" << i + 1 << endl;
+        cin >> valor;
+
+        if(valor % 2 == 0){ // validamos si el valor es par
+            suma = suma + valor;
+        }
+        
+    }
+
+    cout << "La suma es: " << suma << endl;
+    
+    // Usanod while
+
+    cout << "*** Usando while ***" << endl;
+
+    suma = 0;
+
+    while (cont < 5)
+    {
+        cout << "ingrese el valor Num" << cont + 1 << endl;
+        cin >> valor;
+
+        if(valor % 2 == 0){ // Validamos si el valor es par
+            suma = suma + valor;
+        }
+
+        cont++;
+    }
+
+    cout << "La suma es: " << suma << endl;
+
     return 0;
 }
