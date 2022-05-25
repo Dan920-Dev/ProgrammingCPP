f7b85d69ec9e1a2d0c93efd63a003332c5578b68
Dan920-Dev <00138218@uca.edu.sv>
2022-05-25 11:29:42 -0600

    Add main 14

--- Scripts/main14.cpp
+++ Scripts/main14.cpp
@@ -10,9 +10,23 @@ using namespace std;
 
 int main() {
 
-    float precio = 0;
+    float precio = 0, result = 0;
     int cantProduc = 0;
     
+    cout << "Ingrese la cantidad de productos a pagar: " << endl;
+    cin >> cantProduc;
+    
+    for (int i = 0; i < cantProduc; i++)
+    {
+        cout << "Ingrese el precio del producto " << i + 1 << endl;
+        cin >> precio;
+
+        result = result + precio; // result guarda el precio de cada iteracion mas el precio anterior
+
+        precio = 0; // regresamos al valor original el precio
+    }
+
+    cout << "El total a pagar es: $" << result << endl;
     
 
     return 0;
