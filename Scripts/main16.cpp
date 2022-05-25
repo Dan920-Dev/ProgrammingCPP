a99b75940d367a9a974437c505a1aedddc4a9271
Dan920-Dev <00138218@uca.edu.sv>
2022-05-25 11:42:18 -0600

    Add main 16

--- Scripts/main16.cpp
+++ Scripts/main16.cpp
@@ -9,5 +9,36 @@ using namespace std;
 
 int main() {
 
+    cout << "*** Usando for ***" << endl;
+
+    int suma = 0, cont = 0, valor;
+
+    // Usando for
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "ingrese el valor Num" << i + 1 << endl;
+        cin >> valor;
+        suma = suma + valor;
+    }
+
+    cout << "La suma es: " << suma << endl;
+    
+    // Usanod while
+
+    cout << "*** Usando while ***" << endl;
+
+    suma = 0;
+
+    while (cont < 10)
+    {
+        cout << "ingrese el valor Num" << cont + 1 << endl;
+        cin >> valor;
+        suma = suma + valor;
+
+        cont++;
+    }
+
+    cout << "La suma es: " << suma << endl;
+
     return 0;
 }
