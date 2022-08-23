b02b1b5bd1c14263e4f5b59e0234cec37857f64d
Dan920-Dev <00138218@uca.edu.sv>
2022-08-23 08:46:39 -0600

    Add Selection Sort

--- Scripts/main26.cpp
+++ Scripts/main26.cpp
@@ -1,12 +1,67 @@
-// Selection Sort
+// Selection Sort -  Usando Punteros
 
 #include <iostream>
-
 using namespace std;
+
+// función para intercambiar la posición de dos elementos
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+// Funcion para imprimir el Arreglo de forma Ascendente
+void printArrayAs(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+
+// Funcion que muestra arreglo de forma Descendente
+void printArrayDes(int array[], int size) {
+  for (int i = size -1; i >= 0; i--) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void selectionSort(int array[], int size) {
+
+    for (int step = 0; step < size - 1; step++) {
+
+    int min_idx = step;
+
+        for (int i = step + 1; i < size; i++) {
+
+            // Para ordenar en orden descendente
+            // Selecciona el elemento mínimo en cada bucle.
+            if (array[i] < array[min_idx]) {
+                min_idx = i;
+            }
+        
+        }
+
+    
+    swap(&array[min_idx], &array[step]);
+  }
+}
 
 
 int main() {
 
+  int data[] = {8, 4, 3, 10, 22, 2}; // Arreglo a Utilizar
 
-    return 0;
+  int size = sizeof(data) / sizeof(data[0]);
+
+  selectionSort(data, size);
+
+  cout << "Arreglo de forma Ascendente" << endl;
+  printArrayAs(data, size);
+
+  cout << "Arreglo de forma Descendente" << endl;
+  printArrayDes(data, size);
+
+  return 0;
 }
