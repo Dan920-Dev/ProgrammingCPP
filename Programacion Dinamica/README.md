30752b3a67f3fb601da2c625cc0f8ac370b903e3
Dan920-Dev <00138218@uca.edu.sv>
2022-11-24 12:02:44 -0600

    Add examples of Dynamic programming

--- Programacion Dinamica/README.md
+++ Programacion Dinamica/README.md
@@ -1,2 +1,6 @@
 # Algoritmos Programacion Dinamica
 
+1. [FIBONACCI](/Scripts/main54.cpp): Serie fibonacci.
+2. [MAX 1D RANGE SUM](/Scripts/main51.cpp): Dado un arreglo A, determina el rango de posiciones[i,j] del arreglo que al sumar todos sus datos, el resultado es el maximo posible.
+3. [0/1 KNAPSACK PROBLEM](/Scripts/main52.cpp) 
+4. [LCS](/Scripts/main53.cpp) 
---
+++ Scripts/main51.cpp
@@ -0,0 +1,29 @@
+// MAX 1D
+#include <iostream>
+
+using namespace std;
+
+// Se recibe el arreglo y el numero de datos
+int max_1d_range_sum(int* A, int n){
+    int M[n+1], S = 0; // Se declara un arreglo M con un size n+1
+
+    M[0] = 0; // La posicion 0 en ese arreglo sera 0
+    // Para empezar a poblar con los datos recibidos empezamos desde 1 hasta llegar a n
+
+    for(int i = 1; i <= n; i++){
+        
+        S = max( S + A[i-1], A[i-1] );
+        M[i] = max( S, M[i-1] );
+    }
+
+    return M[n];
+}
+
+int main(void){
+    int n = 8;
+    int A[8] = {-2,1,-3,4,-1,2,1,-5};
+
+    cout << "El resultado máximo es: " << max_1d_range_sum(A, n) << ".\n";
+
+    return 0;
+}
